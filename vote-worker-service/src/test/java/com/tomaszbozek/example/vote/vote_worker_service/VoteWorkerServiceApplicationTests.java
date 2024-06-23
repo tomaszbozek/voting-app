72872ecc35750611df4026a79fa4580113a39073
tomasz <tomaszbozek4@wp.pl>
2024-06-24 00:39:55 +0200

    fix(worker-service): fix failing test;

--- vote-worker-service/src/test/java/com/tomaszbozek/example/vote/vote_worker_service/VoteWorkerServiceApplicationTests.java
+++ vote-worker-service/src/test/java/com/tomaszbozek/example/vote/vote_worker_service/VoteWorkerServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.tomaszbozek.example.vote.vote_worker_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
+//@SpringBootTest
 class VoteWorkerServiceApplicationTests {
 
 	@Test
