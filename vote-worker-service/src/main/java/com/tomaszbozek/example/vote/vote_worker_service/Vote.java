e789172e382cbf5e125e0dc0ec835b6902939972
tomasz <tomaszbozek4@wp.pl>
2024-06-29 20:44:56 +0200

    feat(vote-worker-service): generate schema auto;

--- vote-worker-service/src/main/java/com/tomaszbozek/example/vote/vote_worker_service/Vote.java
+++ vote-worker-service/src/main/java/com/tomaszbozek/example/vote/vote_worker_service/Vote.java
@@ -2,6 +2,7 @@ package com.tomaszbozek.example.vote.vote_worker_service;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Data
 @Entity
+@Table(name = "votes")
 public class Vote {
     @Id
     private UUID id;
