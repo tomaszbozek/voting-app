b4b094ee031546d9ce3cee1a88cc74f68f15adff
tomasz <tomaszbozek4@wp.pl>
2024-07-01 16:46:14 +0200

    feat(vote-app): cleanup;

--- vote-worker-service/src/main/java/com/tomaszbozek/example/vote/vote_worker_service/VotesReceiver.java
+++ vote-worker-service/src/main/java/com/tomaszbozek/example/vote/vote_worker_service/VotesReceiver.java
@@ -1,44 +1,32 @@
 package com.tomaszbozek.example.vote.vote_worker_service;
 
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
 public class VotesReceiver {
     private final VotesRepository votesRepository;
-    private final AtomicInteger counter = new AtomicInteger();
     private final ObjectMapper objectMapper;
 
     public void receiveMessage(String message) {
         log.debug("Received <{}>", message);
-        counter.incrementAndGet();
-        log.debug("working...");
-        log.debug("vote: {}", message);
         VoteDto voteDto = asDto(message);
-        log.debug("deserialized value: {}", voteDto);
+        log.debug("Deserialized value: {}", voteDto);
         Vote result = votesRepository.save(new Vote(
                 UUID.randomUUID(),
                 voteDto.name(),
                 voteDto.vote()
         ));
-        log.debug("insert result: {}", result);
+        log.debug("Insert result: {}", result);
     }
 
     @SneakyThrows
     private VoteDto asDto(String message) {
         return objectMapper.readValue(message, VoteDto.class);
-    }
-
-    public int getCount() {
-        return counter.get();
     }
 
     private record VoteDto(String voterId, String name, String vote){}
